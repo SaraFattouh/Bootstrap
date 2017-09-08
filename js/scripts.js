f2d4d834536baac59697c9c7dc7613cdc65179ac
Sara Fattouh <sara.informatics@gmail.com>
2017-09-08 17:47:44 +0300

    Final

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+﻿
+         $(document).ready(function () {
+             $("#mycarousel").carousel({ interval: 2000 });
+
+             $("#carouselPause").click(function () {
+                 $("#mycarousel").carousel('pause')
+             });
+
+             $("#carouselPlay").click(function () {
+                 $("#mycarousel").carousel('cycle')
+             });
+         });
+
+
+         $("#login-link").click(function () {
+             $("#loginModal").modal('show');
+         });
+
+         $("#reserve-link").click(function () {
+             $("#reserve_form").modal('show');
+         });
+
